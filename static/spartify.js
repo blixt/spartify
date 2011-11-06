1e9d8cd046312c4539696a8731e4a2087f873687
Andreas Blixt <blixt@spotify.com>
2011-11-05 22:13:52 -0400

    Interface improvements

--- static/spartify.js
+++ static/spartify.js
@@ -4,26 +4,33 @@ var spartify = function () {
 		this.mock_songs_ = [];
 	}
 	MockApi.prototype.createParty = function (cb) {
+		console.log('MockApi: createParty');
 		setTimeout(function () {
 			cb('ABCDEFG1234');
 		}, 300);
 	}
 	MockApi.prototype.getSongs = function (party, cb) {
+		console.log('MockApi: getSongs', party);
+		var t = this;
 		setTimeout(function () {
-			cb(this.mock_songs_);
+			cb(t.mock_songs_);
 		}, 300);
 	}
-	MockApi.prototype.vote = function (party, song, cb) {
+	MockApi.prototype.vote = function (partyCode, uri, cb) {
+		console.log('MockApi: vote', partyCode, uri);
+		var t = this;
 		setTimeout(function () {
-			if (Math.random() < 0.3 && this.mock_songs_.length > 1) {
-				var i = Math.floor(Math.random() * (this.mock_songs_.length - 1));
-				var song = this.mock_songs_[i];
-				this.mock_songs_[i] = this.mock_songs_[i + 1];
-				this.mock_songs_[i + 1] = song;
+			if (Math.random() < 0.3 && t.mock_songs_.length > 1) {
+				var i = Math.floor(Math.random() * (t.mock_songs_.length - 1));
+				var song = t.mock_songs_[i];
+				t.mock_songs_[i] = t.mock_songs_[i + 1];
+				t.mock_songs_[i + 1] = song;
 			}
-			cb(this.mock_songs_);
+			t.mock_songs_.push(uri);
+			cb(t.mock_songs_);
 		}, 300);
 	}
+
 
 	// The real API.
 	function Api() {
@@ -32,21 +39,32 @@ var spartify = function () {
 		$.getJSON('/api/get_songs', {party: '"' + party + '"'}, cb);
 	}
 
-	/*
-	 * === Interface code. ===
-	 */
+
+	return {
+		api: new MockApi()
+	};
+}();
+
+// Interface code.
+(function () {
 	var partyCode = null;
 
 	function go(page) {
 		$('body').attr('id', 'p-' + page);
+		$('button').attr('disabled', false);
 		$(window).scrollTop(0);
+
+		if (page != 'party') {
+			clearTimeout(timeout);
+			partyCode = null;
+		}
 	}
 
 	var timeout, container = $('#songs');
 	function songsCallback(songs) {
 		container.css('height', songs.length * 40);
 
-		var traversed = [];
+		var lis = container.children('li'), traversed = [];
 		for (var i = 0; i < songs.length; i++) {
 			var uri = songs[i],
 				li = container.children('li[data-uri="' + uri + '"]');
@@ -55,18 +73,25 @@ var spartify = function () {
 				li = $('<li>')
 					.attr('data-uri', uri)
 					.text(uri)
+					.append('<button>+1</button>')
 					.appendTo(container);
 			} else {
-				traversed.push(li);
+				traversed.push(li[0]);
 			}
 
 			li.css('top', i * 40);
 		}
-		container.children('li').not(traversed).remove();
+		lis.not(traversed).remove();
 
 		timeout = setTimeout(getSongs, 1000);
 	}
 
+	function vote(uri) {
+		if (!partyCode) alert('vote without party?');
+		clearTimeout(timeout);
+		spartify.api.vote(partyCode, uri, songsCallback);
+	}
+	
 	function getSongs() {
 		if (!partyCode) return;
 		spartify.api.getSongs(partyCode, songsCallback);
@@ -78,27 +103,44 @@ var spartify = function () {
 		partyCode = code;
 		$('#party-code').html('Party code is: <strong>' + code + '</strong>');
 		go('party');
+
+		getSongs();
 	}
 
+
+	// Main page
 	$('#start').click(function () {
+		$('button').attr('disabled', true);
 		$('#party-code').text('...');
 		spartify.api.createParty(function (code) {
-			history.pushState({page: 'party', partyCode: code}, null, code);
+			history.pushState({page: 'party', partyCode: code}, null, '/' + code);
 			enterParty(code);
 		});
 	});
 
 	$('#go-join').click(function () {
 		go('join');
-		history.pushState({page: 'join'}, null, 'join');
+		history.pushState({page: 'join'}, null, '/join');
 	});
 
+	// Party page
+	$('#add-song').click(function () {
+		vote($('#search').val());
+	});
+
+	$('#songs button').live('click', function () {
+		var uri = $(this).closest('li').data('uri');
+		vote(uri);
+	});
+
+	// Generic
 	$('.go-to-main').click(function () {
 		go('main');
-		history.pushState({page: 'main'}, null, '');
+		history.pushState({page: 'main'}, null, '/');
 	});
 
 
+	go('main');
 	switch (location.pathname) {
 		case '/':
 			break;
@@ -110,8 +152,21 @@ var spartify = function () {
 			break;
 	}
 
+	$(window).bind('popstate', function (e) {
+		var state = e.originalEvent.state;
+		if (!state) return;
 
-	return {
-		api: new MockApi()
-	};
-}();
+		switch (state.page) {
+			case 'join':
+				go('join');
+				break;
+			case 'main':
+				go('main');
+				break;
+			case 'party':
+				$('button').attr('disabled', true);
+				enterParty(state.partyCode);
+				break;
+		}
+	});
+})();
