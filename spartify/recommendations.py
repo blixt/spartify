3686514a43518181acfe69e2fbf1b149d7716319
Ricardo Santos <ricardo@spotify.com>
2011-11-06 00:09:21 -0400

    Spotify metadata lookups first revision.

--- spartify/recommendations.py
+++ spartify/recommendations.py
@@ -2,10 +2,11 @@ import config
 import json
 from urllib import quote_plus as quote
 from google.appengine.api.urlfetch import fetch
+from spartify.track import Track
 
 
 def find_similar_tracks(tracks):
-    res_tracks = set()
+    result = set()
     similar_artists = find_similar_artists(track.artist for track in tracks)
     url = '%ssong/search?api_key=%s&artist_id=%%s&sort=song_hotttnesss-desc&results=1'\
             % (config.ECHONEST_BASE_URL, ECHONEST_API_KEY,)
@@ -20,19 +21,26 @@ def find_similar_tracks(tracks):
                     % (config.SPOTIFY_BASE_URL, quote(query),)
             res = fetch(spotify_url)
             res = json.loads(res.content)
-            res_tracks.add(res['tracks'][0]['href'])
+            # create track
+            res_track = res['tracks'][0]
+            track = Track(res_track['href'])
+            track.set_metadata(
+                res_track['name'],
+                res_track['artists'][0]['name'],
+                res_track['album']['name'])
+            result.add(track)
         except:
             pass
-    return res_tracks
+    return result
 
 
 def find_similar_artists(artists):
-    res_artists = set()
+    result = set()
     url = '%sartist/similar?api_key=%s&name=%%s&results=3'\
             % (config.ECHONEST_BASE_URL, ECHONEST_API_KEY,)
     for artist in artists:
         res = fetch(url % (quote(artist),))
         res = json.loads(res.content)
         for res_artist in res['response']['artists']:
-            res_artists.add(res_artist)
-    return res_artist
+            result.add(res_artist)
+    return result
--- spartify/track.py
+++ spartify/track.py
@@ -14,10 +14,26 @@ class Track:
     def __init__(self, uri):
         self.uri = uri
 
+    def set_metadata(title, artist, album):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self._meta = True
+
+
     def lookup(self):
         if not self._meta:
-            # TODO
-            self._meta = True
+            try:
+                uri = '%slookup/1/.json?uri=%s'
+                res = fetch(spotify_url)
+                res = json.loads(res.content)
+                res_track = res['track']
+                self.set_metadata(
+                    res_track['name'],
+                    res_track['artists'][0]['name'],
+                    res_track['album']['name'])
+            except:
+                pass
 
     def __str__(self):
         return ','.join((self.uri, self.title, self.artist, self.album,))
